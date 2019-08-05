ee3a4a146e555d71f8b940ca6774463b86a3646c
Edward Sugitpibul <etsugitpibul@gmail.com>
2019-08-05 21:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
+}
+
+function whisper(stirng) {
+  return string.toLowerCase();
+}
+
+function logShoutS(string) {
+  console.log().toUpperCase();
 }
