c3f84c872edab6ebf4aa63337b325ce6f2b769ae
Edward Sugitpibul <etsugitpibul@gmail.com>
2019-08-05 21:09:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
   else if(string.toLowerCase() === string) {
-    return "I love you, too"
+    return "I can't hear you!"
   }
+  else if(string)
   
 }
